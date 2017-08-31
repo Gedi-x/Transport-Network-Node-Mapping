d65b859b7bace4f75ff04867e35cd6a906f5237a
KevinZhonghao <kevin.zhonghao.xie@gmail.com>
2017-08-31 20:45:49 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='nodemapping',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http:://github.com/KevinZhonghao/Transport-Network-Node-Mapping',
+      author='KevinZhonghao Xie',
+      author_email='kevin.zhonghao.xie@gmail.com',
+      license='MIT',
+      packages=['Transport-Network-Node-Mapping'],
+      install_requires=[],
+      zip_safe=False)
